0d20435855c00a0a4becfe47845e650a5cc6a744
creativo <camilo.jimenez.r@hotmail.com>
2024-05-27 12:11:13 -0500

    commit27

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const client = new Client({
     authStrategy: new LocalAuth({ clientId: "Client-one" }),
     webVersionCache: {
       type: 'remote',
-      remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html', // Tried 2.2412.54 still same result
+      remotePath: 'https://raw.githubusercontent.com/guigo613/alternative-wa-version/main/html/2.2413.51-beta-alt.html'
     }
   });
 
